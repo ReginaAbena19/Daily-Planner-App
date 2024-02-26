0ca2b17db27255fedf9290c46d44cc41b379db55
Regina <reginaabena.tech@hotmail.com>
2024-02-26 19:13:14 +0000

    [ROB]| Finished js logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 // Display current day 
-$("#currentDay").text(dayjs().format("dddd[,] MMMM DD"));
+$("#currentDay").text(dayjs().format("dddd[,] MMMM DD HH:mm"));
+
+const currentTime = (dayjs().hour());
+
+// Saving daily tasks to local storage
+
+$(".saveBtn").on("click", function(event){
+  event.preventDefault();
+
+  let dailyTask = $(this).prev.val();
+
+  let timeBlock = $(this).attr("time-stamp");
+
+  localStorage.setItem(dailyTask, timeBlock);
+});
+
+// Dynamically Generate time blocks
+for (let hour = 9; hour <= 17; hour++) {
+  $("#timeBlockContainer").append(`
+    <div class="row time-block">
+      <div class="col-2 hour d-flex justify-content-center align-items-center">
+        ${hour > 12 ? hour - 12 + ' PM' : hour + ' AM'}
+      </div>
+      <textarea id="dailyTask${hour}" class="col-8 description"></textarea>
+      <button class="col-2 saveBtn" time-stamp="${hour}"><i class="fas fa-save"></i></button>
+    </div>
+  `);
+}
+
+// Display user inputs into time blocks:
+for (let hour = 9; hour <= 17; hour++) {
+  $(`#dailyTask${hour}`).val(localStorage.getItem(hour.toString()));
+}
+
+$(".time-block").each(function() {
+  let dataHours = $(this).children(".saveBtn").attr("time-stamp");
+  if(dataHours < currentTime) {
+      $(this).children("textarea").addClass("past");
+  } else if(dataHours == currentTime) {
+      $(this).children("textarea").addClass("present");
+  } else {
+      $(this).children("textarea").addClass("future");
+  }
+});
 
