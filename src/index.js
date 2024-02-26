5aad904dc17d6da8be946def040ec6ae7445f377
Regina <reginaabena.tech@hotmail.com>
2024-02-26 20:03:05 +0000

    [ROB]| updating file

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ $(".saveBtn").on("click", function(event){
 
   let timeBlock = $(this).attr("time-stamp");
 
-  localStorage.setItem(dailyTask, timeBlock);
+  localStorage.setItem(timeBlock, dailyTask);
 });
 
 // Dynamically Generate time blocks
-for (let hour = 9; hour <= 20; hour++) {
+for (let hour = 9; hour <= 17; hour++) {
   $("#timeBlockContainer").append(`
     <div class="row time-block">
       <div class="col-2 hour d-flex justify-content-center align-items-center">
-        ${hour > 12 ? hour - 12 + ' PM' : hour + ' AM'}
+      ${hour % 12 === 0 ? 12 : hour % 12} ${hour >= 12 ? 'PM' : 'AM'}
       </div>
       <textarea id="dailyTask${hour}" class="col-8 description"></textarea>
       <button class="col-2 saveBtn" time-stamp="${hour}"><i class="fas fa-save"></i></button>
@@ -29,7 +29,7 @@ for (let hour = 9; hour <= 20; hour++) {
 }
 
 // Display user inputs into time blocks:
-for (let hour = 9; hour <= 20; hour++) {
+for (let hour = 9; hour <= 17; hour++) {
   $(`#dailyTask${hour}`).val(localStorage.getItem(hour.toString()));
 }
 
